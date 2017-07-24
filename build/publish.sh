3dc8b5a6f7a0f96a53cb7f0703b1b70afad42baa
Ian Lovett <i.c.lovett@gmail.com>
2017-07-24 16:23:49 -0700

    add files in publish before checking for dirty

--- build/publish.sh
+++ build/publish.sh
@@ -2,12 +2,13 @@
 
 ROOT_DIR=$(cd $(dirname "$0")/../; pwd)
 
+git -C "$ROOT_DIR" add .
+
 # check if there are any changes to commit
 if git diff-index --quiet HEAD --; then
   echo "no changes detected"
 else
   echo "committing changes..."
-  git -C "$ROOT_DIR" add .
   git -C "$ROOT_DIR" -c user.email="info@rook.io" -c user.name="Rook" commit --message="docs snapshot for rook version `$DOCS_VERSION`"
   echo "pushing changes..."
   git -C "$ROOT_DIR" push
