1ea03a166601eba545b6d418962738c8121738fc
Ian Lovett <i.c.lovett@gmail.com>
2017-07-25 12:57:56 -0700

    updated build script to use more dynamic user and group ids

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,10 @@ CONTAINER_DIR="/opt/rook.github.io"
 
 # install node dependencies through container
 echo "pulling npm dependencies..."
-docker run --rm --user 1000 -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine npm install
+docker run --rm --user $(id -u):$(id -g) -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine npm install
 
 # run prepare script through container
-echo "preparing static assets..."
-docker run -t --user 1000 -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine node build/scripts/preprocess.js
+echo "preprocessing static assets..."
+docker run -t --user $(id -u):$(id -g) -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine node build/scripts/preprocess.js
+
+echo "rook.github.io build complete"
