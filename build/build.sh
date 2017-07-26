52de45696b34f47f9ae11d107b5e2879ba0e65e9
Ian Lovett <i.c.lovett@gmail.com>
2017-07-26 11:51:51 -0700

    add option to specify host user for mapping docker run command

--- build/build.sh
+++ build/build.sh
@@ -3,12 +3,18 @@
 HOST_DIR=$(cd $(dirname "$0")/../; pwd)
 CONTAINER_DIR="/opt/rook.github.io"
 
+if [ -n "$HOST_USER" ]; then
+  DOCKER_RUN="docker run -t --user $HOST_USER:$HOST_USER -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine"
+else
+  DOCKER_RUN="docker run -t -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine"
+fi
+
 # install node dependencies through container
 echo "pulling npm dependencies..."
-docker run -t --user 1000 -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine npm install
+$DOCKER_RUN npm install
 
 # run prepare script through container
 echo "preprocessing static assets..."
-docker run -t --user 1000 -w $CONTAINER_DIR -v $HOST_DIR:$CONTAINER_DIR node:8-alpine node build/scripts/preprocess.js
+$DOCKER_RUN node build/scripts/preprocess.js
 
 echo "rook.github.io build complete"
